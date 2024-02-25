02565db6ca3cd790fdf730d60c7754a442e52aff
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2024-02-25 12:29:10 +0000

    refactor(main): main file only with main function

--- src/main/include/solvers.hpp
+++ src/main/include/solvers.hpp
@@ -1,7 +1,8 @@
 #ifndef _SOLVERS_HPP_
 #define _SOLVERS_HPP_
 
-#include <OPS_solver_t.hpp>
+#include <OPS_bc1.hpp>
+#include <functions.hpp>
 
 template<
   typename T,
@@ -11,8 +12,31 @@ void solve(
   const emir::OpsInput *I, double tol, emir::OPS_output_t &O, std::ostream &os1,
   std::ostream &os2
 ) {
-  T solver_bc(I, O, tol);
-  solver_bc.solve(os1);
+  T solver(I, O, tol);
+  solver.solve(os1);
+}
+
+int processor(
+  const std::string &instance_file_name, const std::string &output_file_name,
+  const std::string &log_file_name, const int id
+) {
+  std::ofstream log_file(log_file_name);
+  std::ofstream output_file(output_file_name);
+  const double tol = 1e-4;
+  const auto model_input = createFromFile<emir::OpsInput>(instance_file_name);
+  emir::OPS_output_t model_output(model_input);
+
+  solve<emir::OPS_cplex_solver1>(
+    &model_input, tol, model_output, log_file, output_file
+  );
+
+  output_file << '\n';
+  output_file.close();
+
+  log_file << '\n';
+  log_file.close();
+
+  return 0;
 }
 
 #endif  // _SOLVERS_HPP_
--- src/main/src/main.cpp
+++ src/main/src/main.cpp
@@ -1,42 +1,10 @@
-#include <cstdio>
-#include <cstdlib>
-#include <fstream>
-
-#include <OPS_bc1.hpp>
 #include <solvers.hpp>
 
-int processor(
-  const std::string &ins_file, const std::string &sta_file,
-  const std::string &log_file, const int id
-) {
-  emir::OpsInput In;
-  std::ifstream input_file(ins_file);
-  input_file >> In;
-  input_file.close();
-
-  const double tol = 1e-4;
-
-  emir::OPS_output_t Out(In);
-
-  std::ofstream O_file(sta_file, std::ios_base::app);
-  std::ofstream L_file(log_file);
-
-  solve<emir::OPS_cplex_solver1>(&In, tol, Out, L_file, O_file);
-
-  O_file << '\n';
-  O_file.close();
-
-  L_file << '\n';
-  L_file.close();
-
-  return 0;
-}
-
 int main(int argc, char *argv[]) {
-  const std::string sta_file("data/temporal.txt");
-  const std::string ins_file("data/test_instance.json");
-  const std::string log_file("data/test_log.txt");
+  const std::string output_file_name("data/temporal.txt");
+  const std::string instance_file_name("data/test_instance.json");
+  const std::string log_file_name("data/test_log.txt");
   const int id = 0;
 
-  return processor(ins_file, sta_file, log_file, id);
+  return processor(instance_file_name, output_file_name, log_file_name, id);
 }
