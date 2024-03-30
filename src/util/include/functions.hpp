1f10df4fdee7a3269a90da9e5b8b41c3c9092a53
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2024-03-30 10:17:45 +0000

    feat: solve function in util folder

--- src/util/include/functions.hpp
+++ src/util/include/functions.hpp
@@ -5,6 +5,7 @@
 #include <fstream>
 
 #include <local_type_traits.hpp>
+#include <ops_solver.hpp>
 
 /**
  * @brief Converts a string to a date stamp.
@@ -28,6 +29,28 @@ Readable createFromFile(const std::string &file_name
   Readable data;
   file >> data;
   return data;
+}
+
+/**
+ * @brief Solve the O.P.S. problem using the given solver.
+ * Also output the log in the given outflow.
+ *
+ * @tparam Solver The type of the solver to be used. It can be any class that
+ * inherits from emir::OpsSolver.
+ * @param input_path The name of the file that has the configuration of the
+ * O.P.S input.
+ * @param tolerance The maximum tolerance to be used in the solver.
+ * @param log_os The outflow where the logs will be stored.
+ */
+template <typename Solver>
+Solver solve(
+  const std::string &input_path, const double tolerance, std::ostream &log_os
+) requires std::is_base_of_v<emir::OpsSolver, Solver> {
+  const auto &instance = createFromFile<emir::OpsInput>(input_path);
+  Solver solver(instance, tolerance);
+  solver.addLog(log_os);
+  solver.solve();
+  return solver;
 }
 
 #endif  // _FUNCTIONS_HPP_
