531179fadd329cf3fce8725bad519d2b1cfaa0ad
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2024-05-11 13:27:39 +0100

    refactor, style(output): check as public method, static constant as constexpr, some variables renamed

--- src/output/src/ops_output.cpp
+++ src/output/src/ops_output.cpp
@@ -40,8 +40,6 @@
 
 namespace emir {
 
-const double OpsOutput::kMaxTimeMargin = 1e-2;
-
 OpsOutput::OpsOutput(const OpsInput &input) :
   input_(std::make_unique<OpsInput>(input)),
   used_arcs_(input.getAmountOfSlidingBars()),
@@ -67,7 +65,8 @@ void OpsOutput::setUsedArcs(const std::vector<double> &used_arcs) {
       if (isEqual(value, 0.0)) { continue; }
       if (!isEqual(value, 1.0)) {
         throw OpsError(
-          "Invalid value for used arc: {}. It must be 1 or 0.", value
+          "Invalid value for used arc ({} -> {}): {}. It must be 1 or 0.",
+          arc.getOriginId(), arc.getDestinationId(), value
         );
       }
       used_arcs_[graph_idx][arc.getOriginId()] = arc.getDestinationId();
@@ -77,31 +76,36 @@ void OpsOutput::setUsedArcs(const std::vector<double> &used_arcs) {
 
 void OpsOutput::setObservedObjects(const std::vector<double> &observed_objects
 ) {
+  if (observed_objects.empty()) {
+    throw OpsError("The visited objects must be set.");
+  }
   const auto amount_of_objects = input_->getAmountOfObjects();
   observed_objects_[0] = true;
   observed_objects_[amount_of_objects - 1] = true;
-  if (observed_objects.empty()) { return; }
   for (int idx = 1; idx < amount_of_objects - 1; ++idx) {
     const double value = observed_objects[idx - 1];
-    if (!isEqual(value, 0.0) && !isEqual(value, 1.0)) {
+    if (isEqual(value, 0.0)) { continue; }
+    if (!isEqual(value, 1.0)) {
       throw OpsError(
-        "Invalid value for visited object: {}. It must be 1 or 0.", value
+        "Invalid value for object {}: {}. It must be 1 or 0.", idx, value
       );
     }
-    observed_objects_[idx] = isEqual(value, 1.0);
+    observed_objects_[idx] = true;
   }
 }
 
 void OpsOutput::setTimeAtObjects(const std::vector<double> &time_at_objects) {
   const auto amount_of_objects = input_->getAmountOfObjects();
   time_at_objects_[0] = 0;
-  if (time_at_objects.empty()) { return; }
+  if (time_at_objects.empty()) {
+    throw OpsError("The accumulated time at each node must be set.");
+  }
   for (auto idx = 1; idx < amount_of_objects; ++idx) {
     const double value = time_at_objects[idx - 1];
     if (value < 0) {
       throw OpsError(
         "Invalid value for object {} with time {}. It must be non-negative.",
-        value, idx
+        idx, value
       );
     }
     time_at_objects_[idx] = value / input_->getScalingFactor();
@@ -121,13 +125,13 @@ OpsOutput &OpsOutput::operator=(const OpsOutput &output) {
 }
 
 std::ostream &operator<<(std::ostream &output_stream, const OpsOutput &output) {
-  output_stream << nlohmann::json({{"x", output.used_arcs_},
-                                   {"y", output.observed_objects_},
-                                   {"s", output.time_at_objects_},
-                                   {"profit", output.getTotalProfit()},
-                                   {"time_elapsed", output.time_elapsed_}}
-  ).dump(2);
-  return output_stream;
+  return output_stream << nlohmann::json(
+                            {{"x", output.used_arcs_},
+                             {"y", output.observed_objects_},
+                             {"s", output.time_at_objects_},
+                             {"profit", output.getTotalProfit()},
+                             {"time_elapsed", output.time_elapsed_}}
+         ).dump(2);
 }
 
 // ------------------------------- Getters --------------------------------- //
@@ -146,8 +150,8 @@ std::pair<std::vector<int>, std::vector<int>>
 OpsOutput::countArrivesAndDepartures() const {
   const auto amount_of_objects = input_->getAmountOfObjects();
   const auto amount_of_sliding_bars = input_->getAmountOfSlidingBars();
-  std::vector<int> amount_of_arrives(amount_of_objects, 0);
-  std::vector<int> amount_of_departures(amount_of_objects, 0);
+  std::vector<int> arrives_per_node(amount_of_objects, 0);
+  std::vector<int> departures_per_node(amount_of_objects, 0);
 
   for (int graph_idx = 0; graph_idx < amount_of_sliding_bars; ++graph_idx) {
     const auto &graph = input_->getGraph(graph_idx);
@@ -156,12 +160,12 @@ OpsOutput::countArrivesAndDepartures() const {
         .origin_id = arc.getOriginId(), .destination_id = arc.getDestinationId()
       };
       if (arcWasUsed(graph_idx, arc_endpoints)) {
-        amount_of_arrives[arc_endpoints.destination_id]++;
-        amount_of_departures[arc_endpoints.origin_id]++;
+        arrives_per_node[arc_endpoints.destination_id]++;
+        departures_per_node[arc_endpoints.origin_id]++;
       }
     }
   }
-  return {amount_of_arrives, amount_of_departures};
+  return {arrives_per_node, departures_per_node};
 }
 
 // -------------------------------- Checks -------------------------------- //
@@ -174,31 +178,31 @@ void OpsOutput::check() const {
 void OpsOutput::checkArcs() const {
   const auto amount_of_objects = input_->getAmountOfObjects();
   const auto amount_of_sliding_bars = input_->getAmountOfSlidingBars();
-  const auto [amount_of_arrives, amount_of_departures] =
+  const auto [arrives_per_node, departures_per_node] =
     countArrivesAndDepartures();
 
-  if (amount_of_departures[0] != amount_of_sliding_bars) {
+  if (departures_per_node[0] != amount_of_sliding_bars) {
     throw OpsError(
       "The first node must be visited in each sliding bar. It has {} visits.",
-      amount_of_departures[0]
+      departures_per_node[0]
     );
   }
-  if (amount_of_arrives[amount_of_objects - 1] != amount_of_sliding_bars) {
+  if (arrives_per_node[amount_of_objects - 1] != amount_of_sliding_bars) {
     throw OpsError(
       "The last node must be visited in each sliding bar. It has {} visits.",
-      amount_of_arrives[amount_of_objects - 1]
+      arrives_per_node[amount_of_objects - 1]
     );
   }
 
   for (std::size_t idx = 1; idx < amount_of_objects - 1; ++idx) {
-    if (amount_of_departures[idx] != amount_of_arrives[idx]) {
+    if (departures_per_node[idx] != arrives_per_node[idx]) {
       throw OpsError(
         "Node {} must have the same number of arrival and departure arcs. "
         "It has {} arrivals and {} departures.",
-        idx, amount_of_arrives[idx], amount_of_departures[idx]
+        idx, arrives_per_node[idx], departures_per_node[idx]
       );
     }
-    if (observed_objects_[idx] != (amount_of_arrives[idx] > 0)) {
+    if (observed_objects_[idx] != (arrives_per_node[idx] > 0)) {
       throw OpsError(
         "Node {} must be visited in order to have arrival / departure arcs.",
         idx
